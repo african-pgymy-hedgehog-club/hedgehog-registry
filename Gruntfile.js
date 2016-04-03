fad0f566c11221be127a5deaf4d8eacd32fad5ec
Scott Crossan <s.crossan@crownoil.co.uk>
2016-04-03 11:46:39 +0100

    Added production and development options for register-hedgehog bundle to reduce size in production

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,38 @@
 module.exports = function (grunt) {
 
-	function browserifyOptions(port) {
+	function browserifyOptions(port, live) {
 		port = port || 4774;
+		live = live || false
 
-		var options = {
-			browserifyOptions: {
-				debug: true
-			},
-			watch: true,
-			keepAlive: true,
-			transform: [
-				[{}, 'babelify', {
-					loose: 'all'
-				}],
-				[{global: true}, 'envify']
-			],
-			plugin: [
-				['livereactload', {port: port}]
-			]
-		};
+		if(!live) {
+			var options = {
+				browserifyOptions: {
+					debug: true
+				},
+				watch: true,
+				keepAlive: true,
+				transform: [
+					[{}, 'babelify', {
+						loose: 'all'
+					}],
+					[{global: true}, 'envify']
+				],
+				plugin: [
+					['livereactload', {port: port}]
+				]
+			};
+		}
+		else if(live) {
+			var options = {
+				watch: true,
+				keepAlive: true,
+				transform: [
+					[{}, 'babelify', {
+						loose: 'all'
+					}]
+				]
+			};
+		}
 
 		return options;
 	}
@@ -31,26 +45,32 @@ module.exports = function (grunt) {
 				src: 'js/app.js',
 				dest: 'js/bundle.js'
 			},
-			hedgehogRegistry: {
+			hedgehogRegistryDev: {
 				options: browserifyOptions(4477),
+				src: 'components/register-hedgehog.js',
+				dest: 'js/register-hedgehog.bundle.js'
+			},
+			hedgehogRegistryLive: {
+				options: browserifyOptions(4477, true),
 				src: 'components/register-hedgehog.js',
 				dest: 'js/register-hedgehog.bundle.js'
 			}
 		},
 
 		env: {
-			build: {
-				NODE_ENV: 'development'
+			dist: {
+		        NODE_ENV : 'production',
 			},
-			production: {
-				NODE_ENV: 'production'
+		    dev: {
+				NODE_ENV: 'development',
 			}
 		}
     });
 
 	// Default task
 	grunt.registerTask('default', ['env:build', 'browserify:app']);
-	grunt.registerTask('hedgehog-registry', ['env:build', 'browserify:hedgehogRegistry']);
+	grunt.registerTask('hedgehog-registry-dev', ['env:dev', 'browserify:hedgehogRegistryDev']);
+	grunt.registerTask('hedgehog-registry-live', ['env:dist', 'browserify:hedgehogRegistryLive']);
 
 	// Load up tasks
 	grunt.loadNpmTasks('grunt-browserify');
